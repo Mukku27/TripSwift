47411497398a4a20ba2671e348587e9631f90010
sai-satish <saisatish.nilla1@gmail.com>
2024-12-26 02:13:26 +0530

    Resolved Env file conflict

---
+++ lib/secrets.dart
@@ -0,0 +1,7 @@
+final FIREBASE_API_KEY = 'AIzaSyBN6WxdMncs_nFof2wzkjukUkWuZjz009w';
+final FIREBASE_APP_ID= '1:108336513808:web:1ec1369da2540ce5171b95';
+final FIREBASE_MESSAGING_SENDER_ID=  '108336513808';
+final FIREBASE_PROJECT_ID= 'tripswift-cd1d9';
+final FIREBASE_AUTH_DOMAIN= 'tripswift-cd1d9.firebaseapp.com';
+final FIREBASE_STORAGE_BUCKET= 'tripswift-cd1d9.firebasestorage.app';
+final FIREBASE_MEASUREMENT_ID= 'G-QY0EJSD2EX';
