3649955dab5b0026226037dbcf3f61666c1fa3a0
sai-satish <saisatish.nilla1@gmail.com>
2024-12-26 02:16:29 +0530

    Removed env references

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:trip_swift/secrets.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -54,23 +55,23 @@ class DefaultFirebaseOptions {
 
   static FirebaseOptions get web {
     return FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_API_KEY']!,
-      appId: dotenv.env['FIREBASE_APP_ID']!,
-      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
-      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
-      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
+      apiKey: FIREBASE_API_KEY,
+      appId: FIREBASE_APP_ID,
+      messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+      projectId: FIREBASE_PROJECT_ID,
+      authDomain: FIREBASE_AUTH_DOMAIN,
+      storageBucket: FIREBASE_STORAGE_BUCKET,
+      measurementId: FIREBASE_MEASUREMENT_ID,
     );
   }
 
   static FirebaseOptions get android {
     return FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_API_KEY']!,
-      appId: dotenv.env['FIREBASE_APP_ID']!,
-      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      apiKey: FIREBASE_API_KEY,
+      appId: FIREBASE_APP_ID,
+      messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+      projectId: FIREBASE_PROJECT_ID,
+      storageBucket: FIREBASE_STORAGE_BUCKET,
     );
   }
 }
